59f95100f5453f4a7c532743d5b5ebedd58a4185
félix chamerois <brulelame@hotmail.fr>
2016-10-08 14:36:20 +0200

    push 14:36 08/10

---
+++ Assets/testgrass.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testgrass : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter (Collider col)
+    {
+        if (col.tag=="Player") { 
+        Destroy(gameObject);
+        }
+    }
+}
