6be9211a6a3cbaca16e286e09e94c10cacdcf51d
félix chamerois <brulelame@hotmail.fr>
2016-10-09 23:09:43 +0200

    main menu

---
+++ Assets/loadscene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour
+{
+    public GameObject projectile;
+    void Update()
+    {
+        if (Input.GetButtonDown("Boost_J1"))
+            SceneManager.LoadScene("principale");
+    }
+}
