7eba90ae59c64ed9fa8c8e7f369bfdbf78a05a38
Antoine Tirmarche <goneril.dalbany@gmail.com>
2016-10-12 16:36:35 +0200

    Modif Ecran Titre Selection de map + Reverse Map

--- Assets/loadscene.cs
+++ Assets/loadscene.cs
@@ -8,7 +8,10 @@ public class loadscene : MonoBehaviour
     public  UnityEngine.AudioSource vroum;
     public AudioClip ready;
     public AudioClip bipChangementJoueur;
+	public AudioClip bipchangementMap;
+	private string Map = "Canyon";
     public Text nbJoueur;
+	public Text NomMap;
     private int nombreJoueur = 4;
 
     private float timer;
@@ -20,10 +23,16 @@ public class loadscene : MonoBehaviour
     
     void Update()
     {
-        if (Input.GetButtonDown("Boost_J1")) {
-        vroum.PlayOneShot(ready, 2.0f);
-            SceneManager.LoadScene("principale"+nombreJoueur.ToString()+"J");
-        
+		if (Input.GetButtonDown("Boost_J1"))
+		{
+			if (Map == "Canyon"){
+				vroum.PlayOneShot(ready, 2.0f);
+				SceneManager.LoadScene("principale"+nombreJoueur.ToString()+"J");
+			}			
+			if (Map == "RCanyon"){
+				vroum.PlayOneShot(ready, 2.0f);
+				SceneManager.LoadScene("Reverse"+nombreJoueur.ToString()+"J");
+			}
         }
 
         if (Input.GetAxis("upNbJoueur")>0)
@@ -47,8 +56,35 @@ public class loadscene : MonoBehaviour
                 vroum.PlayOneShot(bipChangementJoueur);
             }
             
-        }
+		}        
+		if (Input.GetAxis("leftMapSelect")>0)
+		{
+			if(Time.time - timer > 0.2f)
+			{
+				timer = Time.time;
+				if (Map=="Canyon"){
+				Map = "RCanyon";
+				vroum.PlayOneShot(bipchangementMap);
+				NomMap.text= Map;
+			}
 
+				}
+
+		}
+		if (Input.GetAxis("leftMapSelect")<0)
+		{
+			if(Time.time - timer > 0.2f)
+			{
+				timer = Time.time;
+				if (Map=="RCanyon"){
+					Map = "Canyon";
+					vroum.PlayOneShot(bipchangementMap);
+					NomMap.text= Map;
+				}
+
+			}
+
+		}
     }
 
     private string setTextJoueur()
