6cba6f8c50625d7d557b8c298a8f2af4318fde3f
Antoine Tirmarche <goneril.dalbany@gmail.com>
2016-10-16 17:36:48 +0200

    Script SetUp ship fini.

--- Assets/SetUpShip.cs
+++ Assets/SetUpShip.cs
@@ -20,6 +20,9 @@ public class SetUpShip : MonoBehaviour {
     private AudioSource Source1, Source2, Source3, Source4;
     //Sources des boost et des impacts
     private AudioSource BSource1, BSource2, BSource3, BSource4;
+    //P1, P2 et cie
+    public GameObject PS1, PS2, PS3, PS4;
+    private Vector3 Pos1, Pos2, Pos3, Pos4;
 
     // Use this for initialization
     void Awake()
@@ -40,6 +43,8 @@ public class SetUpShip : MonoBehaviour {
         affectsystemcourse();
         //régler le panoramique des audiosource
         setpanoramic();
+        //changer les icones
+        affectPlayerIcon();
     }
 	
 	// Update is called once per frame
@@ -74,22 +79,22 @@ public class SetUpShip : MonoBehaviour {
         if (IndexVaisseau == 1)
         {
             Object.Instantiate(V1);
-            GameObject.Find("Vaisseau"+IndexVaisseau.ToString()+"(Clone)").transform.parent = JoueurContainer.transform;
+            GameObject.Find("Vaisseau" + IndexVaisseau.ToString() + "(Clone)").transform.parent = JoueurContainer.transform;
         }
         if (IndexVaisseau == 2)
         {
             Object.Instantiate(V2);
-            GameObject.Find("Vaisseau" + IndexVaisseau.ToString()+"(Clone)").transform.parent = JoueurContainer.transform;
+            GameObject.Find("Vaisseau" + IndexVaisseau.ToString() + "(Clone)").transform.parent = JoueurContainer.transform;
         }
         if (IndexVaisseau == 3)
         {
             Object.Instantiate(V3);
-            GameObject.Find("Vaisseau" + IndexVaisseau.ToString()+"(Clone)").transform.parent = JoueurContainer.transform;
+            GameObject.Find("Vaisseau" + IndexVaisseau.ToString() + "(Clone)").transform.parent = JoueurContainer.transform;
         }
         if (IndexVaisseau == 4)
         {
             Object.Instantiate(V4);
-            GameObject.Find("Vaisseau" + IndexVaisseau.ToString()+"(Clone)").transform.parent = JoueurContainer.transform;
+            GameObject.Find("Vaisseau" + IndexVaisseau.ToString() + "(Clone)").transform.parent = JoueurContainer.transform;
         }
     }
 
@@ -192,10 +197,13 @@ public class SetUpShip : MonoBehaviour {
 
     void affectsystemcourse()
     {
-        SystemCourse.GetComponent<SystemCourse>().vaisseauxJoueurs[0] = GameObject.Find("Joueur 1/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ1.ToString() + "(Clone)");
-        SystemCourse.GetComponent<SystemCourse>().vaisseauxJoueurs[1] = GameObject.Find("Joueur 2/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ2.ToString() + "(Clone)");
-        SystemCourse.GetComponent<SystemCourse>().vaisseauxJoueurs[2] = GameObject.Find("Joueur 3/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ3.ToString() + "(Clone)");
-        SystemCourse.GetComponent<SystemCourse>().vaisseauxJoueurs[3] = GameObject.Find("Joueur 4/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ4.ToString() + "(Clone)");
+        if (IndexJoueur == 1)
+        {
+            SystemCourse.GetComponent<SystemCourse>().vaisseauxJoueurs[0] = GameObject.Find("Joueur 1/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ1.ToString() + "(Clone)");
+            SystemCourse.GetComponent<SystemCourse>().vaisseauxJoueurs[1] = GameObject.Find("Joueur 2/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ2.ToString() + "(Clone)");
+            SystemCourse.GetComponent<SystemCourse>().vaisseauxJoueurs[2] = GameObject.Find("Joueur 3/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ3.ToString() + "(Clone)");
+            SystemCourse.GetComponent<SystemCourse>().vaisseauxJoueurs[3] = GameObject.Find("Joueur 4/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ4.ToString() + "(Clone)");
+        }
     }
 
     void setpanoramic()
@@ -209,18 +217,23 @@ public class SetUpShip : MonoBehaviour {
         }
         if (Info.GetComponent<GetInfo>().NbJoueur == 2)
         {
-            Source1 = GameObject.Find("Joueur 1/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ1.ToString() + "(Clone)/J" + Info.GetComponent<GetInfo>().vaisseauJ1.ToString()).GetComponent<AudioSource>();
-            Source1.panStereo = -1.0f;
-            Source2 = GameObject.Find("Joueur 2/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ2.ToString() + "(Clone)/J" + Info.GetComponent<GetInfo>().vaisseauJ2.ToString()).GetComponent<AudioSource>();
-            Source2.panStereo = 1.0f;
-            BSource1 = GameObject.Find("Joueur 1/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ1.ToString() + "(Clone)/J" + Info.GetComponent<GetInfo>().vaisseauJ1.ToString() + "/SJ" + Info.GetComponent<GetInfo>().vaisseauJ1.ToString()).GetComponent<AudioSource>();
-            BSource1.panStereo = -1.0f;
-            BSource2 = GameObject.Find("Joueur 2/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ2.ToString() + "(Clone)/J" + Info.GetComponent<GetInfo>().vaisseauJ2.ToString() + "/SJ" + Info.GetComponent<GetInfo>().vaisseauJ2.ToString()).GetComponent<AudioSource>();
-            BSource2.panStereo = 1.0f;
+            if (IndexJoueur == 1)
+            {
+                Source1 = GameObject.Find("Joueur 1/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ1.ToString() + "(Clone)/J" + Info.GetComponent<GetInfo>().vaisseauJ1.ToString()).GetComponent<AudioSource>();
+                Source1.panStereo = -1.0f;
+                Source2 = GameObject.Find("Joueur 2/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ2.ToString() + "(Clone)/J" + Info.GetComponent<GetInfo>().vaisseauJ2.ToString()).GetComponent<AudioSource>();
+                Source2.panStereo = 1.0f;
+                BSource1 = GameObject.Find("Joueur 1/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ1.ToString() + "(Clone)/J" + Info.GetComponent<GetInfo>().vaisseauJ1.ToString() + "/SJ" + Info.GetComponent<GetInfo>().vaisseauJ1.ToString()).GetComponent<AudioSource>();
+                BSource1.panStereo = -1.0f;
+                BSource2 = GameObject.Find("Joueur 2/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ2.ToString() + "(Clone)/J" + Info.GetComponent<GetInfo>().vaisseauJ2.ToString() + "/SJ" + Info.GetComponent<GetInfo>().vaisseauJ2.ToString()).GetComponent<AudioSource>();
+                BSource2.panStereo = 1.0f;
+            }
         }
         if (Info.GetComponent<GetInfo>().NbJoueur == 3)
         {
-            Source1 = GameObject.Find("Joueur 1/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ1.ToString() + "(Clone)/J" + Info.GetComponent<GetInfo>().vaisseauJ1.ToString()).GetComponent<AudioSource>();
+            if (IndexJoueur == 1)
+            {
+                Source1 = GameObject.Find("Joueur 1/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ1.ToString() + "(Clone)/J" + Info.GetComponent<GetInfo>().vaisseauJ1.ToString()).GetComponent<AudioSource>();
             Source1.panStereo = -1.0f;
             Source2 = GameObject.Find("Joueur 2/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ2.ToString() + "(Clone)/J" + Info.GetComponent<GetInfo>().vaisseauJ2.ToString()).GetComponent<AudioSource>();
             Source2.panStereo = 1.0f;
@@ -232,25 +245,65 @@ public class SetUpShip : MonoBehaviour {
             BSource2.panStereo = 1.0f;
             BSource3 = GameObject.Find("Joueur 3/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ3.ToString() + "(Clone)/J" + Info.GetComponent<GetInfo>().vaisseauJ3.ToString() + "/SJ" + Info.GetComponent<GetInfo>().vaisseauJ3.ToString()).GetComponent<AudioSource>();
             BSource3.panStereo = -0.2f;
+            }
         }
         if (Info.GetComponent<GetInfo>().NbJoueur == 4)
         {
-            Source1 = GameObject.Find("Joueur 1/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ1.ToString() + "(Clone)/J" + Info.GetComponent<GetInfo>().vaisseauJ1.ToString()).GetComponent<AudioSource>();
-            Source1.panStereo = -1.0f;
-            Source2 = GameObject.Find("Joueur 2/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ2.ToString() + "(Clone)/J" + Info.GetComponent<GetInfo>().vaisseauJ2.ToString()).GetComponent<AudioSource>();
-            Source2.panStereo = 1.0f;
-            Source3 = GameObject.Find("Joueur 3/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ3.ToString() + "(Clone)/J" + Info.GetComponent<GetInfo>().vaisseauJ3.ToString()).GetComponent<AudioSource>();
-            Source3.panStereo = -0.6f;
-            Source4 = GameObject.Find("Joueur 4/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ4.ToString() + "(Clone)/J" + Info.GetComponent<GetInfo>().vaisseauJ4.ToString()).GetComponent<AudioSource>();
-            Source4.panStereo = 0.6f;
-            BSource1 = GameObject.Find("Joueur 1/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ1.ToString() + "(Clone)/J" + Info.GetComponent<GetInfo>().vaisseauJ1.ToString() + "/SJ" + Info.GetComponent<GetInfo>().vaisseauJ1.ToString()).GetComponent<AudioSource>();
-            BSource1.panStereo = -1.0f;
-            BSource2 = GameObject.Find("Joueur 2/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ2.ToString() + "(Clone)/J" + Info.GetComponent<GetInfo>().vaisseauJ2.ToString() + "/SJ" + Info.GetComponent<GetInfo>().vaisseauJ2.ToString()).GetComponent<AudioSource>();
-            BSource2.panStereo = 1.0f;
-            BSource3 = GameObject.Find("Joueur 3/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ3.ToString() + "(Clone)/J" + Info.GetComponent<GetInfo>().vaisseauJ3.ToString() + "/SJ" + Info.GetComponent<GetInfo>().vaisseauJ3.ToString()).GetComponent<AudioSource>();
-            BSource3.panStereo = -0.6f;
-            BSource4 = GameObject.Find("Joueur 4/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ4.ToString() + "(Clone)/J" + Info.GetComponent<GetInfo>().vaisseauJ3.ToString() + "/SJ" + Info.GetComponent<GetInfo>().vaisseauJ4.ToString()).GetComponent<AudioSource>();
-            BSource4.panStereo = 0.6f;
+            if (IndexJoueur == 1)
+            {
+                Source1 = GameObject.Find("Joueur 1/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ1.ToString() + "(Clone)/J" + Info.GetComponent<GetInfo>().vaisseauJ1.ToString()).GetComponent<AudioSource>();
+                Source1.panStereo = -1.0f;
+                Source2 = GameObject.Find("Joueur 2/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ2.ToString() + "(Clone)/J" + Info.GetComponent<GetInfo>().vaisseauJ2.ToString()).GetComponent<AudioSource>();
+                Source2.panStereo = 1.0f;
+                Source3 = GameObject.Find("Joueur 3/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ3.ToString() + "(Clone)/J" + Info.GetComponent<GetInfo>().vaisseauJ3.ToString()).GetComponent<AudioSource>();
+                Source3.panStereo = -0.6f;
+                Source4 = GameObject.Find("Joueur 4/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ4.ToString() + "(Clone)/J" + Info.GetComponent<GetInfo>().vaisseauJ4.ToString()).GetComponent<AudioSource>();
+                Source4.panStereo = 0.6f;
+                BSource1 = GameObject.Find("Joueur 1/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ1.ToString() + "(Clone)/J" + Info.GetComponent<GetInfo>().vaisseauJ1.ToString() + "/SJ" + Info.GetComponent<GetInfo>().vaisseauJ1.ToString()).GetComponent<AudioSource>();
+                BSource1.panStereo = -1.0f;
+                BSource2 = GameObject.Find("Joueur 2/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ2.ToString() + "(Clone)/J" + Info.GetComponent<GetInfo>().vaisseauJ2.ToString() + "/SJ" + Info.GetComponent<GetInfo>().vaisseauJ2.ToString()).GetComponent<AudioSource>();
+                BSource2.panStereo = 1.0f;
+                BSource3 = GameObject.Find("Joueur 3/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ3.ToString() + "(Clone)/J" + Info.GetComponent<GetInfo>().vaisseauJ3.ToString() + "/SJ" + Info.GetComponent<GetInfo>().vaisseauJ3.ToString()).GetComponent<AudioSource>();
+                BSource3.panStereo = -0.6f;
+                BSource4 = GameObject.Find("Joueur 4/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ4.ToString() + "(Clone)/J" + Info.GetComponent<GetInfo>().vaisseauJ3.ToString() + "/SJ" + Info.GetComponent<GetInfo>().vaisseauJ4.ToString()).GetComponent<AudioSource>();
+                BSource4.panStereo = 0.6f;
+            }
         }
+    }
+
+    void affectPlayerIcon()
+    {
+        if(IndexJoueur == 1)
+        {
+            Object.Instantiate(PS1);
+            GameObject.Find("particle_p1(Clone)").transform.parent = GameObject.Find("Joueur 1/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ1.ToString() + "(Clone)").transform;
+            //Pos1 = GameObject.Find("Joueur 1/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ1.ToString() + "(Clone)").transform.position;
+            GameObject.Find("particle_p1(Clone)").transform.position = GameObject.Find("Joueur 1/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ1.ToString() + "(Clone)").transform.position;
+            GameObject.Find("particle_p1(Clone)").transform.Translate(0, 10, Time.deltaTime);
+        }
+        if (IndexJoueur == 2)
+        {
+            Object.Instantiate(PS2);
+            GameObject.Find("particle_p2(Clone)").transform.parent = GameObject.Find("Joueur 2/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ2.ToString() + "(Clone)").transform;
+            GameObject.Find("particle_p2(Clone)").transform.position = GameObject.Find("Joueur 2/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ2.ToString() + "(Clone)").transform.position;
+            GameObject.Find("particle_p2(Clone)").transform.Translate(0, 10, Time.deltaTime);
+
+        }
+        if (IndexJoueur == 3)
+        {
+            Object.Instantiate(PS3);
+            GameObject.Find("particle_p3(Clone)").transform.parent =  GameObject.Find("Joueur3/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ3.ToString() + "(Clone)").transform;
+            GameObject.Find("particle_p3(Clone)").transform.position = GameObject.Find("Joueur 3/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ3.ToString() + "(Clone)").transform.position;
+            GameObject.Find("particle_p3(Clone)").transform.Translate(0, 10, Time.deltaTime);
+        }
+       
+        if (IndexJoueur == 4)
+        {
+            Object.Instantiate(PS4);
+            GameObject.Find("particle_p4(Clone)").transform.parent = GameObject.Find("Joueur 4/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ4.ToString() + "(Clone)").transform;
+            GameObject.Find("particle_p4(Clone)").transform.position = GameObject.Find("Joueur 4/Vaisseau" + Info.GetComponent<GetInfo>().vaisseauJ4.ToString() + "(Clone)").transform.position;
+            GameObject.Find("particle_p4(Clone)").transform.Translate(0, 10, Time.deltaTime);
+        }
+      
     }
 }
